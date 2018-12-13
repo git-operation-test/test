30875da6ad9f8c2ad260e4eddc650b87845d2d98
git-operation-test <45850886+git-operation-test@users.noreply.github.com>
2018-12-13 23:32:02 +0900

    009 add hello test!

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+    std::cout << "Hello, Test!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
